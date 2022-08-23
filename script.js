9aada4aaeae9c359314d8ee7d4edaf71d03bc39e
Refuse94 <simko.tomas94@gmail.com>
2022-08-23 17:04:11 +0200

    functions to scrollTOp

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Ahoj")
+// ============ FUNKCIE NA SCROLLOVANIE ODKAZOV NA STRANKE ===========================
+
+let aboutButton = document.querySelector(".about_button");
+let aboutHash = document.querySelector("#about").offsetTop;
+
+
+aboutButton.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scrollTo({
+        top: aboutHash,
+        behavior: "smooth"
+    })
+})
+
+let portfolioButton = document.querySelector(".portfolio_button");
+let portfolioHash = document.querySelector("#portfolio").offsetTop;
+
+portfolioButton.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scrollTo({
+        top: portfolioHash,
+        behavior: "smooth"
+    })
+})
