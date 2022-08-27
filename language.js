c761fa4d70cc1ef69b3df91293d084df61320d74
Refuse94 <simko.tomas94@gmail.com>
2022-08-27 15:30:47 +0200

    add retard system of language, need try some new

---
+++ language.js
@@ -0,0 +1,52 @@
+const englishButton = document.querySelector(".slovak")
+const slovakButton = document.querySelector(".eng")
+
+const aboutSlovak ={
+    a: "O mne"
+}
+const homeSlovak = {
+    a: "Domov"
+}
+const contactSlovak = {
+    a: "Kontakt"
+}
+const reviewsSlovak = {
+    a: "Recenzie"
+}
+
+const homeTextSlovak ={
+    h2: "Ahoj volám sa",
+    h3: "Som creatívna dizajnérka zo Slovenska",
+    h4: "Som kreatívna dizajnérka so sídlom na Slovensku a som veľmi zanietená a oddaná svojej práci "
+}
+
+const aButton = document.querySelector(".about_button")
+const homeButton = document.querySelector(".home_button")
+const cButton = document.querySelector(".contact_button")
+const reviewsButton = document.querySelector(".reviews_button")
+const homeText = document.querySelector("#home_text h2")
+const homeTextH3 = document.querySelector("#home_text h3")
+const homeTextH4 = document.querySelector("#home_text h4")
+const aboutME = document.querySelector(".social_icons .about")
+console.log(homeText)
+
+
+
+englishButton.addEventListener("click", function(){
+    
+    aButton.textContent = aboutSlovak.a;
+    homeButton.textContent= homeSlovak.a;
+    cButton.textContent= contactSlovak.a;
+    reviewsButton.textContent= reviewsSlovak.a;
+    homeText.textContent= homeTextSlovak.h2;
+    homeTextH3.textContent = homeTextSlovak.h3;
+    homeTextH4.textContent = homeTextSlovak.h4;
+    aboutME.textContent= aboutSlovak.a;
+   
+
+    
+})
+
+slovakButton.addEventListener("click", function(){
+    window.location.reload();
+})
