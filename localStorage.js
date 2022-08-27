be8c299535abf4feea3e7d92cf09a3cb500982c3
Refuse94 <simko.tomas94@gmail.com>
2022-08-27 10:57:31 +0200

    update LocalStorage

--- localStorage.js
+++ localStorage.js
@@ -1,4 +1,5 @@
 let contactFormular = document.querySelector(".contact_formular");
+// PODMIENKA NECH SA MI POLE NEVYTVARA ODZNOVA PRI ZNOVU NACITANI STRANKY
 
 if (localStorage.getItem("Users") == null) {
     var customers = []
@@ -7,46 +8,51 @@ if (localStorage.getItem("Users") == null) {
 }
 
 
+// ODCHYTENIE MOJICH INPUTOV
+const formular = document.querySelector("#form")
+const myName = document.querySelector(".my_name");
+const myEmayl = document.querySelector(".my_emayl");
+const myPhone = document.querySelector(".my_phone");
+const mySubject = document.querySelector(".my_subject");
+const myMessage = document.querySelector(".my_message");
 
+// SUBMIT FUNKCIA 
 contactFormular.addEventListener("submit", function (event) {
     event.preventDefault()
+    // VALUE Z INPUTOV
     let firstName = event.target.firstName.value
     let yourEmail = event.target.email.value
     let yourPhone = event.target.yourPhone.value
     let yourSubject = event.target.yourSubject.value
     let yourMessage = event.target.message.value
 
-    let result = " [ " + firstName + " ] " + yourEmail + " | " + yourPhone + " | " + yourSubject + " | " + yourMessage;
-    customers.push(result);
-
-// const obj ={
-//     age:
-//     meno: firstName
-
-// }
-
-// const arr = [obj1, obj2]
-
-// arr[1].meno
-
-    // customers.push(firstName, yourEmail, yourMessage, yourPhone, yourSubject);
-
-
-
-
+    // PUSH DO POLA
+    customers.push({myName: firstName, myEmayl: yourEmail, myPhone: yourPhone, mySubject: yourSubject, myMessage: yourMessage});
+    
+    // UKLADANIE DO LOCAL STORAGE
     customersJ = JSON.stringify(customers);
     localStorage.setItem("Users", customersJ);
 
+    // VYPIS Z LOCAL STORAGE NA S5
     let customersFromLS = localStorage.getItem("Users")
     let myUserFromLS = JSON.parse(customersFromLS);
 
     console.log(myUserFromLS);
-
-    console.log(customers[0]);
-
-
-
-
-
-    // console.log("odoslalo sa to" + " " + firstName + " " + yourEmail + " " + yourPhone+ " " + yourSubject+ " " + yourMessage);
+    
+    // VYMAZANIE INPUTOV PO ODOSLANI SUBMITU
+    event.target.firstName.value = ""
+    event.target.email.value = ""
+    event.target.yourPhone.value = ""
+    event.target.yourSubject.value = ""
+    event.target.message.value = ""
 })
+
+
+
+
+
+
+
+
+
+
